d523a4d4a7939144fe762f7a9c4cce1a5b2e7dae
jose200112 <josemunozmunoz128@gmail.com>
2026-04-15 15:01:12 +0200

    feat: dockerize frontend with Nginx
    - Multistage Dockerfile: Node 20 build + Nginx serve
    - nginx.conf with Angular Router support
    - environment.ts and environment.production.ts
    - All hardcoded localhost URLs replaced with environment variables
    - angular.json configured with fileReplacements for production

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  userServiceUrl: 'http://localhost:8080',
+  teacherServiceUrl: 'http://localhost:8081',
+  bookingServiceUrl: 'http://localhost:8082',
+  notificationServiceUrl: 'http://localhost:8083',
+  chatServiceUrl: 'http://localhost:8084'
+};
